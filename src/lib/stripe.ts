ae6615714a1b3f523ca7b44e24d4e45f24ac0bc0
sasmb <ogbsamueljm@gmail.com>
2025-03-27 01:09:11 +0200

    chore: update Stripe API version to 2025-02-24.acacia

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,6 +5,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-02-24.acacia',
   typescript: true,
 }); 
